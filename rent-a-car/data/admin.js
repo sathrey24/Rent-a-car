655d0e170c3c24e8d80430e7d9bf9c0ed6b26b61
Priyanka <apple@Priyanka.local>
2021-11-29 01:15:45 -0500

    added userDashboard and adminDashboard views

--- rent-a-car/data/admin.js
+++ rent-a-car/data/admin.js
@@ -1,25 +1,36 @@
 const mongoCollections = require('../config/mongoCollections');
 const admin = mongoCollections.admin;
+const bcrypt = require('bcrypt');
+const saltRounds = 16;
 
 module.exports = {
-    async createAdmin(firstName, lastName, username, password, phoneNumber, email){
+    async createAdmin(firstName, lastName,username, password, phoneNumber, email ){
+        const hashPasswd = await bcrypt.hash(password, saltRounds);
         let newAdmin = {
             firstName:firstName,
             lastName:lastName,
-            username: username,
-            password: password,
+            username: username.toLowerCase(),
+            password: hashPasswd,
             role: "admin",
             phoneNumber: phoneNumber,
             email: email
         }
         const adminCollection = await admin()
+        await adminCollection.insertOne(newAdmin);
+    },
+
+    async checkUser(username, password){
+        let compareToMatch = false;
+        if (!username || !password) 
+        throw 'All fields need to have valid values';
+        const adminCollection = await admin();
         const res = await adminCollection.findOne({ username: username.toLowerCase() });
-        if (res != null) throw "Admin with this username already exist";
-        const insertInfo = await adminCollection.insertOne(newAdmin);
-        if (insertInfo.insertedCount === 0){
-            throw "Internal Server Error"
-        } else {
-            return {adminInserted: true}
+        if (res === null) throw "Either the username or password is invalid";
+        compareToMatch = await bcrypt.compare(password, res.password);
+        if(compareToMatch){
+          return {authenticated: true,role : res.role};
+        }else{
+          throw "Either the username or password is invalid";
         }
-    }
+      }
 };
