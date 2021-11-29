1d44aa8fa032bc439afbe554e5a7076f8718a961
Priyanka <apple@Priyanka.local>
2021-11-29 01:15:45 -0500

    added userDashboard and adminDashboard views

--- rent-a-car/routes/index.js
+++ rent-a-car/routes/index.js
@@ -1,9 +1,11 @@
 const userRoutes = require('./users');
 const carRoutes = require('./cars');
+//const adminRoutes = require('./admin');
 
 const constructorMethod = (app) => {
   app.use('/', userRoutes);
-  app.use('/car', carRoutes);
+  app.use('/cars', carRoutes);
+  //app.use('/admin', adminRoutes);
 
   app.use('*', (req, res) => {
     res.status(404).render('user/error', {error: "<p> We're sorry, this page could not be found </p>"});
