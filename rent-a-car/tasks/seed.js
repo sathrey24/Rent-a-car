2f56f73a580a1c9ad9b2a666d4a966b79822805f
sanjayathrey <sathrey@stevens.edu>
2021-12-04 13:27:25 -0500

    added reviews to seed and dynamic avg overall rating

--- rent-a-car/tasks/seed.js
+++ rent-a-car/tasks/seed.js
@@ -5,18 +5,28 @@ const data = require('../data/');
 async function main(){
     const db = await dbConnection();
     // create user
-    await data.users.createUser("Sanjay", "Athrey", "123 Stevens Ave", "sathrey@stevens.edu", "111-111-1111", "A11111111111111", "sathrey@stevens.edu", "password1")
-    await data.users.createUser("Priyanka", "Chaurasia", "456 Stevens Ave", "pchauras@stevens.edu", "222-222-2222", "A22222222222222", "pchauras@stevens.edu", "password2")
-    await data.users.createUser("Anjali", "Paliwal", "789 Stevens Ave", "apaliwa1@stevens.edu", "333-333-3333", "A33333333333333", "apaliwa1@stevens.edu", "password3")
+    const sanjay = await data.users.createUser("Sanjay", "Athrey", "123 Stevens Ave", "sathrey@stevens.edu", "111-111-1111", "A11111111111111", "sathrey@stevens.edu", "password1")
+    const priyanka = await data.users.createUser("Priyanka", "Chaurasia", "456 Stevens Ave", "pchauras@stevens.edu", "222-222-2222", "A22222222222222", "pchauras@stevens.edu", "password2")
+    const anjali = await data.users.createUser("Anjali", "Paliwal", "789 Stevens Ave", "apaliwa1@stevens.edu", "333-333-3333", "A33333333333333", "apaliwa1@stevens.edu", "password3")
     
     //create admin
     await data.admin.createAdmin("Patrick", "Hill", "phill@stevens.edu", "password4", "444-444-4444", "phill@stevens.edu")
     
     //create cars
-    await data.cars.createCar("2018 Honda Civic", "Sedan", "black", "4", "5", "30$/hr", true, "gas")
-    await data.cars.createCar("2020 Audi A6", "Sedan", "GREY", "4", "5", "60$/hr", true, "gas")
-    await data.cars.createCar("2019 Nissan Altima", "Coupe", "blue", "2", "4", "40$/hr", true, "gas")
-    await data.cars.createCar("2021 Tesla Model S", "Sedan", "red", "4", "5", "70$/hr", true, "electric")
+    const honda = await data.cars.createCar("2018 Honda Civic", "Sedan", "black", "4", "5", "30$/hr", true, "gas")
+    await data.reviews.createReview(sanjay.userId, honda.carId, "This car was awesome!", 5)
+    await data.reviews.createReview(priyanka.userId, honda.carId, "This car was horrible!", 1)
+    const audi = await data.cars.createCar("2020 Audi A6", "Sedan", "GREY", "4", "5", "60$/hr", true, "gas")
+    await data.reviews.createReview(anjali.userId, audi.carId, "This car was amazing!", 4)
+    const nissan = await data.cars.createCar("2019 Nissan Altima", "Coupe", "blue", "2", "4", "40$/hr", true, "gas")
+    const tesla = await data.cars.createCar("2021 Tesla Model S", "Sedan", "red", "4", "5", "70$/hr", true, "electric")
+    await data.reviews.createReview(priyanka.userId, nissan.carId, "This car was ok!", 3)
+    await data.reviews.createReview(priyanka.userId, tesla.carId, "This car was outstanding!", 5)
+
+
+
+
+
     await db.serverConfig.close()
 
 }
