01590f3c3d09b69311b7608b6fe01ef8f893b757
sanjayathrey <sathrey@stevens.edu>
2021-12-05 11:35:21 -0500

    added requests to seed

--- rent-a-car/tasks/seed.js
+++ rent-a-car/tasks/seed.js
@@ -12,17 +12,20 @@ async function main(){
     //create admin
     await data.admin.createAdmin("Patrick", "Hill", "phill@stevens.edu", "password4", "444-444-4444", "phill@stevens.edu")
     
-    //create cars
+    //create cars, reviews, and requests to rent
     const honda = await data.cars.createCar("2018 Honda Civic", "Sedan", "black", "4", "5", "30$/hr", true, "gas")
     await data.reviews.createReview(sanjay.userId, honda.carId, "This car was awesome!", 5)
+    await data.requests.createRequest(sanjay.userId, honda.carId)
     await data.reviews.createReview(priyanka.userId, honda.carId, "This car was horrible!", 1)
     const audi = await data.cars.createCar("2020 Audi A6", "Sedan", "GREY", "4", "5", "60$/hr", true, "gas")
     await data.reviews.createReview(anjali.userId, audi.carId, "This car was amazing!", 4)
+    await data.requests.createRequest(anjali.userId, audi.carId)
     const nissan = await data.cars.createCar("2019 Nissan Altima", "Coupe", "blue", "2", "4", "40$/hr", true, "gas")
+    await data.requests.createRequest(sanjay.userId,nissan.carId)
     const tesla = await data.cars.createCar("2021 Tesla Model S", "Sedan", "red", "4", "5", "70$/hr", true, "electric")
     await data.reviews.createReview(priyanka.userId, nissan.carId, "This car was ok!", 3)
     await data.reviews.createReview(priyanka.userId, tesla.carId, "This car was outstanding!", 5)
-
+    await data.requests.createRequest(priyanka.userId, tesla.carId)
 
 
 
