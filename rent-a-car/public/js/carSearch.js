6d3542810921e381e0a6957dbcefcb4db64ae4ab
Priyanka <apple@Priyanka.local>
2021-12-08 22:22:33 -0500

    Fixed date selection validation while booking car

--- rent-a-car/public/js/carSearch.js
+++ rent-a-car/public/js/carSearch.js
@@ -34,6 +34,11 @@ function search_car() {
     }
 }
 
+function onCountChange(){
+    document.getElementById("timePeriod").value = "None";
+    document.getElementById("total_cost").value = "";
+}
+
 function onSelectionChange(element) {
     var numberofdays_element = document.getElementById("numberofdays");
     var numberofdays_element = document.getElementById("numberofdays");
@@ -65,45 +70,51 @@ function onFromDateChange(element){
     let selectedYear = parseInt(element.value.substr(0,4))
     let selectedMonth = parseInt(element.value.substr(5,2))
     if(dateObj == "Invalid Date") throw "Invalid Date";
-    if(selectedDay != day || selectedMonth != month || selectedYear != year) {
+    if(selectedYear === year || selectedYear < year ){
+    if(selectedDay < day || selectedMonth < month || selectedYear < year) {
         errorDiv.hidden = false;
         errorDiv.innerHTML = "From date should be current or future date only";
+        element.value = "";
         document.getElementById('from_date').focus();
     }else{
         errorDiv.hidden = true;
-    }
+    }}
 }
 
 function onToDateChange(element){
     let errorDiv = document.getElementById('id_error1');
     var dateObj = new Date();
-    var month = dateObj.getMonth() + 1; //months from 1-12
-    var day = dateObj.getDate();
-    var year = dateObj.getFullYear();
+    var count_element = document.getElementById("numberofdays");
+    var fromDate_element = document.getElementById("from_date");
+    var numberofdays_element = document.getElementById("timePeriod");
     if (!element.value){
         errorDiv.hidden = false;
         errorDiv.innerHTML = "To date should be current or future date only";
+        element.value = "";
         document.getElementById('to_date').focus();
     }
-    let selectedDay = parseInt(element.value.substr(8,9))
-    let selectedYear = parseInt(element.value.substr(0,4))
-    let selectedMonth = parseInt(element.value.substr(5,2))
     if(dateObj == "Invalid Date") throw "Invalid Date";
-    if (selectedYear < year){
-        errorDiv.hidden = false;
-        errorDiv.innerHTML = "To date should be current or future date only";
-        document.getElementById('to_date').focus();
+    if(numberofdays_element.value === "Hour"){
+        if(fromDate_element.value !== element.value){
+            errorDiv.hidden = false;
+            errorDiv.innerHTML = "To date should be same as From date as Time Span is selected in Hours";
+            element.value = "";
+            document.getElementById('to_date').focus();
+        }else{
+            errorDiv.hidden = true;
+        }
     }
-    if (selectedYear > year){
-        errorDiv.hidden = true;
-    }
-    if (selectedYear === year){
-        if (selectedDay >= day && selectedMonth >= month){
-                errorDiv.hidden = true;
-            } else {
-                errorDiv.hidden = false;
-                errorDiv.innerHTML = "To date should be current or future date only";
-                document.getElementById('to_date').focus();
-            }
-    }
+    else if(numberofdays_element.value === "Day"){
+        let fromDateValue = new Date(new Date(fromDate_element.value).getTime()+(1*24*60*60*1000));
+        let expectedDate = new Date(new Date(fromDateValue).getTime()+(count_element.value*24*60*60*1000));
+        let selectedDate = new Date(new Date(element.value).getTime()+(1*24*60*60*1000));
+        if(selectedDate.getDate() != expectedDate.getDate() || selectedDate.getDate() != expectedDate.getDate() || selectedDate.getDate() != expectedDate.getDate()){
+            errorDiv.hidden = false;
+            errorDiv.innerHTML = "To Date "+element.value+" does not match to the number of days selected in Time Span from the From Date";
+            element.value = "";
+            document.getElementById('to_date').focus();
+        }else{
+            errorDiv.hidden = true;
+        }
+}
 }
